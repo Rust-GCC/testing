913b4896c75400197de92e86793b5db4916ccd04
CohenArthur <arthur.cohen@epita.fr>
2022-03-11 20:39:26 +0100

    ftf: Remove extra UTF-8 failing tests

--- gccrs-ui.sh
+++ gccrs-ui.sh
@@ -6,7 +6,7 @@ yml=gccrs-ui.yml.new
 if [ "$1" = "prepare" ]; then
   mkdir $gccrs_ui_dir || echo "Skipping directory creation"
 
-  for file in $(find src/test/ui/ -name '*.rs'); do
+  for file in $(find rust/src/test/ui/ -name '*.rs'); do
     printf "\rCopying file to location: $gccrs_ui_dir/$file"
     mkdir -p $gccrs_ui_dir/$(dirname $file)
     cp $file $gccrs_ui_dir/$file
